0cf495adc9bfcb7b4287e0d2614ae243ad70e5ca
kexitus <kirillb6543211@gmail.com>
2023-05-13 23:51:16 +0700

    Init commit

---
+++ frontend/src/app/create/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CreateHabitForm from '@/container/form/CreateHabitForm';
+
+const CreateHabitPage = () => {
+  return (
+    <div className="content content_fill">
+      <CreateHabitForm />
+    </div>
+  );
+};
+
+export default CreateHabitPage;
