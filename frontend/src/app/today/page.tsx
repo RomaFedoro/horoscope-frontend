5b9a1371aae751190f03c31870e6fb54dbec7380
RomaFedoro <fedorovroman1506@gmail.com>
2023-05-15 19:13:20 +0700

    Start develop Today page

---
+++ frontend/src/app/today/page.tsx
@@ -0,0 +1,13 @@
+import TodayHabitList from '@/container/TodayHabitList';
+import { getToday } from '@/utils/getToday';
+
+export default function TodayPage() {
+  const todayDate = getToday();
+
+  return (
+    <div className="content content_fill">
+      <h1>{todayDate}</h1>
+      <TodayHabitList />
+    </div>
+  );
+}
