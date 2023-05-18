3edcdeecb91657afd8bce102f1d35dbf2620007d
DaVas255 <DaVas255@gmail.com>
2023-05-18 07:25:07 +0700

    List separation in Today template

---
+++ frontend/src/components/HabitItem/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import HabitCheckbox from '@/components/ui/HabitCheckbox';
+import styles from './styles.module.scss';
+import { THabitItem, THabitItemProps } from '@/types/habit';
+import cn from 'classnames';
+import Link from 'next/link';
+
+const HabitItem = ({
+  id,
+  name,
+  onlyRead = false,
+  ...habit
+}: THabitItemProps) => {
+  if (onlyRead)
+    return (
+      <div className={styles.habit}>
+        <Link href={`/habits/${id}`} className={styles.habit__title}>
+          {name}
+        </Link>
+      </div>
+    );
+
+  const { totalSteps, completedSteps, onChange } = habit as THabitItem;
+  const needRepeat = totalSteps - completedSteps;
+  const handleCheckboxChange = (habitId: number | string) =>
+    onChange({
+      completedSteps:
+        totalSteps > completedSteps ? completedSteps + 1 : completedSteps - 1,
+      habitId,
+    });
+
+  return (
+    <div
+      className={cn(styles.habit, needRepeat! <= 0 && styles.habit_completed)}
+    >
+      <HabitCheckbox
+        onChange={() => handleCheckboxChange(id)}
+        needRepeat={needRepeat!}
+      />
+      <Link href={`/habits/${id}`} className={styles.habit__title}>
+        {name}
+      </Link>
+    </div>
+  );
+};
+
+export default HabitItem;
