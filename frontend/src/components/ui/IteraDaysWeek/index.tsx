2c898e428915e9450a9f2ca6054274792f60944e
RomaFedoro <fedorovroman1506@gmail.com>
2023-05-22 09:23:44 +0700

    feat(menu): added active state for menu item

---
+++ frontend/src/components/ui/IteraDaysWeek/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React, { memo } from 'react';
+import { DAYS_OF_WEEK } from '@/constants/daysweek';
+import IteraCheckbox from '@/components/ui/IteraCheckbox';
+
+type TIteraDaysWeekProps = {
+  onChange: (value: Day) => void;
+  defaultValues: Day[];
+};
+
+const IteraDaysWeek = ({ defaultValues, onChange }: TIteraDaysWeekProps) => {
+  return (
+    <div className="list-row">
+      {DAYS_OF_WEEK.map(({ value, label }) => {
+        return (
+          <IteraCheckbox
+            key={value}
+            onChange={() => onChange(value)}
+            checked={defaultValues.includes(value)}
+          >
+            {label.toUpperCase()}
+          </IteraCheckbox>
+        );
+      })}
+    </div>
+  );
+};
+
+export default memo(IteraDaysWeek);
